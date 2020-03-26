55dfdad8088060219f292420d7af5e09184efba3
Chris Cantrell <topherCantrell@gmail.com>
2020-03-26 07:02:27 -0500

    updates

---
+++ blink.asm
@@ -0,0 +1,24 @@
+._CPU = 8051
+
+.include 8051_SFR.asm
+
+0x0000:
+START:
+
+    MOV    P2,#0xFF
+    ACALL  DELAY
+
+    MOV    P2,#0x00
+    ACALL  DELAY
+
+    SJMP   START
+
+DELAY:
+    MOV    R2,#5
+    MOV    R3,#0
+    MOV    R4,#0
+DEL01:
+    DJNZ   R4,DEL01
+    DJNZ   R3,DEL01
+    DJNZ   R2,DEL01
+    RET
